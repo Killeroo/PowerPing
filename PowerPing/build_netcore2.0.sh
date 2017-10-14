4fe0dae6cddc77c6512198d4fd5368ce0e6cd66b
Killeroo <matthewcarney64@gmail.com>
2017-10-14 18:17:54 +0100

    Added PowerPing/PowerPing_netcore1.0.csproj Added build scripts for net core 1.0 and 2.0

---
+++ PowerPing/build_netcore2.0.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dotnet restore PowerPing_netcore2.0.csproj
+
+dotnet build PowerPing_netcore2.0.csproj
+
+dotnet publish -c Release -r win-x64
+dotnet publish -c Release -r win-x86
+dotnet publish -c Release -r osx.10.12-x64
+dotnet publish -c Release -r linux-x64
