53f5a798cb0520525a481d8227d7b604a769bc64
Matthew Carney <matthewcarney64@gmail.com>
2017-10-17 11:27:13 +0100

    Update build_netcore2.0.sh

--- PowerPing/build_netcore2.0.sh
+++ PowerPing/build_netcore2.0.sh
@@ -4,7 +4,7 @@ dotnet restore PowerPing_netcore2.0.csproj
 
 dotnet build PowerPing_netcore2.0.csproj
 
-dotnet publish -c Release -r win-x64
-dotnet publish -c Release -r win-x86
-dotnet publish -c Release -r osx.10.12-x64
-dotnet publish -c Release -r linux-x64
+dotnet publish PowerPing_netcore2.0.csproj -c Release -r win-x64
+dotnet publish PowerPing_netcore2.0.csproj -c Release -r win-x86
+dotnet publish PowerPing_netcore2.0.csproj -c Release -r osx.10.12-x64
+dotnet publish PowerPing_netcore2.0.csproj -c Release -r linux-x64
