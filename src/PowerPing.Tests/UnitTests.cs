80284ef8e22f4e63477c4f805bb83c3b6188cd2a
Killeroo <matthewcarney64@gmail.com>
2018-01-28 00:10:26 +0000

    Changed to NUnit Added example console testing implementation

--- src/PowerPing.Tests/UnitTests.cs
+++ src/PowerPing.Tests/UnitTests.cs
@@ -1,45 +1,107 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using PowerPing;
 
+using NUnit.Framework;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
 namespace PowerPing.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTests
     {
-        [TestMethod]
-        public void TestAttributes()
+        TextWriter m_normalOutput;
+        StringWriter m_testingConsole;
+        StringBuilder m_testingSB;
+
+        #region test fixture setup/teardown methods
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
         {
-            PingAttributes testAttrs = new PingAttributes();
+            // Set current folder to testing folder
+            string assemblyCodeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            string dirName = Path.GetDirectoryName(assemblyCodeBase);
 
-            testAttrs.Address = "127.0.0.1";
-            testAttrs.Count = 1;
-            testAttrs.Interval = 100;
-            testAttrs.Message = "testest";
+            if (dirName.StartsWith("file:\\"))
+                dirName = dirName.Substring(6);
 
-            Ping p = new Ping();
+            Environment.CurrentDirectory = dirName;
 
-            PingResults results = p.SendICMP(testAttrs);
-
-            Assert.AreEqual(testAttrs, p.Attributes);
+            m_testingSB = new StringBuilder();
+            m_testingConsole = new StringWriter(m_testingSB);
+            m_normalOutput = System.Console.Out;
+            System.Console.SetOut(m_testingConsole);
         }
 
-        [TestMethod]
-        public void TestLocalhostPing()
+        [OneTimeTearDown]
+        public void TestFixtureTearDown()
         {
-            PingAttributes testAttrs = new PingAttributes();
-
-            testAttrs.Address = "127.0.0.1";
-            testAttrs.Count = 3;
-            testAttrs.Interval = 100;
-
-            Ping p = new Ping();
-
-            PingResults results = p.SendICMP(testAttrs);
-
-            Assert.AreEqual(results.Received, 3);
+            System.Console.SetOut(m_normalOutput);
         }
-        
+        #endregion
+
+        [SetUp]
+        public void SetUp()
+        {
+            // clear string builder
+            m_testingSB.Remove(0, m_testingSB.Length);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Verbose output in console
+            m_normalOutput.Write(m_testingSB.ToString());
+        }
+
+        [Test]
+        public void ShowCmdHelpIfNoArguments()
+        {
+            // Check exit is normal
+            Assert.AreEqual(0, StartConsoleApplication(""));
+
+            // Check that help information shown correctly.
+            Assert.IsTrue(m_testingSB.ToString().Contains("Advanced ping utility"));//Program.COMMAND_LINE_HELP));
+        }
+
+        /// &lt;span class="code-SummaryComment">&lt;summary>&lt;/span>
+        /// Starts the console application.
+        /// &lt;span class="code-SummaryComment">&lt;/summary>&lt;/span>
+        /// &lt;span class="code-SummaryComment">&lt;param name="arguments">The arguments for console application. &lt;/span>
+        /// Specify empty string to run with no arguments&lt;/param />
+        /// &lt;span class="code-SummaryComment">&lt;returns>exit code&lt;/returns>&lt;/span>
+        private int StartConsoleApplication(string arguments)
+        {
+            // Initialize process here
+            Process proc = new Process();
+            proc.StartInfo.FileName = "PowerPing.exe";
+            // add arguments as whole string
+            proc.StartInfo.Arguments = arguments;
+
+            // use it to start from testing environment
+            proc.StartInfo.UseShellExecute = false;
+
+            // redirect outputs to have it in testing console
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+
+            // set working directory
+            proc.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+
+            // start and wait for exit
+            proc.Start();
+            proc.WaitForExit();
+
+            // get output to testing console.
+            System.Console.WriteLine(proc.StandardOutput.ReadToEnd());
+            System.Console.Write(proc.StandardError.ReadToEnd());
+
+            // return exit code
+            return proc.ExitCode;
+        }
+
     }
 }
