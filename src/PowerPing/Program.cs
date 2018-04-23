52c8069209fbc7595e2966e0555e51395f8bbc45
Killeroo <matthewcarney64@gmail.com>
2018-04-23 10:50:02 +0100

    updated other copyright year

--- src/PowerPing/Program.cs
+++ src/PowerPing/Program.cs
@@ -5,7 +5,7 @@
 /*
 MIT License - PowerPing 
 
-Copyright (c) 2017 Matthew Carney
+Copyright (c) 2018 Matthew Carney
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
