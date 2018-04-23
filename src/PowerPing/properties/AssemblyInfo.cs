9777a35348346b7af72a1c3b57e1548c97a30f1d
Killeroo <matthewcarney64@gmail.com>
2018-04-23 10:28:10 +0100

    Updated assembly info copyright year

--- src/PowerPing/properties/AssemblyInfo.cs
+++ src/PowerPing/properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Matthew Carney")]
 [assembly: AssemblyProduct("PowerPing")]
-[assembly: AssemblyCopyright("Copyright © Matthew Carney 2017")]
+[assembly: AssemblyCopyright("Copyright © Matthew Carney 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
