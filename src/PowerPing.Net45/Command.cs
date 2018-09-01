7c0a6992d27bb4a5b8e43cc9c5a576fbfc5a7d92
Killeroo <matthewcarney64@gmail.com>
2018-09-01 12:53:27 +0100

    Added some new classes

---
+++ src/PowerPing.Net45/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerPing
+{
+    /// <summary>
+    /// Contains command line utilities (such as commands parsing etc)
+    /// </summary>
+    class Command
+    {
+    }
+}
---
+++ src/PowerPing.Net45/SendTasks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerPing
+{
+    /// <summary>
+    /// Contains async methods for sending and recieving
+    /// </summary>
+    class SendTasks
+    {
+    }
+}
