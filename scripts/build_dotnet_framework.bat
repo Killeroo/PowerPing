9fa63b34d68c1216f5073868030182fd834bf293
Matthew Carney <matthewcarney64@gmail.com>
2019-07-16 00:11:28 +0100

    Added test-template.ps1 scripts Updated build_dotnet_framework.bat (doesn't work)

--- scripts/build_dotnet_framework.bat
+++ scripts/build_dotnet_framework.bat
@@ -1,24 +1,20 @@
 @echo off
 
 :: Arguments check
-if [%~1]==[] (
-    echo.
-    echo ERROR: Not enough Arguments
-    echo USAGE: build.bat [C:\path\to\project]
-    exit /b 1
-)
-set projectPath=%~f1
+::if [%~1]==[] (
+::    echo.
+::    echo ERROR: Not enough Arguments
+::    echo USAGE: build.bat [C:\path\to\project]
+::    exit /b 1
+::)
+::set projectPath=%~f1
 
 :: Set .NET 4.6 framework path
 set msbuildPath="%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\"
 
-:: Save starting directory
-set origDir=%~dp0
-
-:: Goto msbuild dir
-cd %msbuildPath%
-
 :: Run build command
-msbuild.exe %projectPath%\PowerPing.sln /p:configuration=release
+cd..
+%msbuildPath%\msbuild.exe PowerPing.sln /p:configuration=release /p:Platform="x86"
+%msbuildPath%\msbuild.exe PowerPing.sln /p:configuration=release /p:Platform="x64"
 
 pause
---
+++ scripts/test-template.ps1
@@ -0,0 +1,12 @@
+﻿#$processPath = (Get-Location).Path+'C:\Projects\PowerPing\src\PowerPing.Net45\bin\Debug\PowerPing.exe'
+#Sending input to running process https://stackoverflow.com/a/16100200
+
+$Result = Start-Process 'C:\Projects\PowerPing\src\PowerPing.Net45\bin\Debug\PowerPing.exe' -ArgumentList '-noinput -badargument' -PassThru -Wait
+
+Write-Host "Test bad argument: " -NoNewline
+if($Result.ExitCode -eq 0) {
+	# True, last operation succeeded
+    Write-Host("==== Test passed =====") -ForegroundColor DarkGreen
+} else {
+    Write-Host("--- Test Failed ---") -ForegroundColor DarkRed
+}
