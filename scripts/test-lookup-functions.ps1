8c56d5e452c89eecac7bf2f83b819322554c654e
matthew carney <matthewcarney64@gmail.com>
2019-11-05 22:32:16 +0000

    Added test-lookup-functions.ps1
    This represents the basic premise for how I am going to do testing with PowerPing.
    Basically we are going to load the PowerPing assembly into the powershell test script, then we will access the internal functions that we want to test.
    We can check the return values (a few of the lookup functions have been converted to return strings instead of sending stuff to stout, which is probably for the good to start splitting up display and functionality anyway) and perform psuedo unit tests.
    
    I know we could setup actual unit tests in C# but I want to avoid getting stuck into the minutia of writing tests in proper OO so writing the tests in powershell makes them easy to run, fairly easy to write and also easier to debug and not get caught up on

---
+++ scripts/test-lookup-functions.ps1
@@ -0,0 +1,12 @@
+﻿# Load the PowerPing assembly into powershell
+$lib = [Reflection.Assembly]::LoadFile("C:\Repos\PowerPing\PowerPing.exe");
+
+Write-Host("Test GetLocalAddress(): ") -NoNewline;
+$powershellLocalAddress = (ipconfig | findstr "IPv4").split(":").replace(" ", "")[1]
+$powerpingLocalAddress = [PowerPing.Lookup]::GetLocalAddress();
+if ($powershellLocalAddress -eq $powerpingLocalAddress) {
+    Write-Host("==== Test passed =====") -ForegroundColor Green
+} else {
+    Write-Host("--- Test Failed ---") -ForegroundColor Red 
+}
+# Check the other arguments returned in ipconfig
