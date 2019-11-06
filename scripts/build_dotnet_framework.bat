b53652b1be2d461236617917fdf5ccdcf342f69e
matthew carney <matthewcarney64@gmail.com>
2019-11-06 20:42:06 +0000

    Switch build script to use visual studio msbuild instead 4.0 msbuild

--- scripts/build_dotnet_framework.bat
+++ scripts/build_dotnet_framework.bat
@@ -1,5 +1,7 @@
 @echo off
 
+cd "%~dp0" 
+
 :: Arguments check
 ::if [%~1]==[] (
 ::    echo.
@@ -10,11 +12,12 @@
 ::set projectPath=%~f1
 
 :: Set .NET 4.6 framework path
-set msbuildPath="%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\"
+set windows_msbuild_path="%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\"
+set vs_msbuild_path="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\msbuild.exe"
 
 :: Run build command
 cd..
-%msbuildPath%\msbuild.exe PowerPing.sln /p:configuration=release /p:Platform="x86"
-%msbuildPath%\msbuild.exe PowerPing.sln /p:configuration=release /p:Platform="x64"
-
-pause
+::%windows_msbuild_path%\msbuild.exe PowerPing.sln /p:Configuration=Release /p:Platform="x86"
+::%windows_msbuild_path%\msbuild.exe PowerPing.sln /p:Configuration=Release /p:Platform="x64"
+%vs_msbuild_path% PowerPing.sln /p:Configuration=Release /p:Platform="x86"
+%vs_msbuild_path% PowerPing.sln /p:Configuration=Release /p:Platform="x64"
