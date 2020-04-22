278e44d72b85202028fbe5eb5abd90c319079a84
Matthew Carney <matthewcarney64@gmail.com>
2020-04-22 21:03:40 +0100

    Update Flood.cs to use new Ping class properly

--- src/Commands/Flood.cs
+++ src/Commands/Flood.cs
@@ -35,15 +35,17 @@ namespace PowerPing
     /// </summary>
     static class Flood
     {
+        private static RateLimiter displayUpdateLimiter = new RateLimiter(TimeSpan.FromMilliseconds(500));
+        private static ulong previousPingsSent = 0;
+        private static string m_Address = "";
+
         public static void Start(string address, CancellationToken cancellationToken)
         {
             PingAttributes attrs = new PingAttributes();
-            RateLimiter displayUpdateLimiter = new RateLimiter(TimeSpan.FromMilliseconds(500));
-            ulong previousPingsSent = 0;
-            Ping p = new Ping(cancellationToken);
+            m_Address = address;
 
             // Verify address
-            attrs.Address = Lookup.QueryDNS(address, AddressFamily.InterNetwork); 
+            attrs.ResolvedAddress = Lookup.QueryDNS(address, AddressFamily.InterNetwork);
 
             // Setup ping attributes
             attrs.Interval = 0;
@@ -51,35 +53,38 @@ namespace PowerPing
             attrs.Message = "R U Dead Yet?";
             // TODO: Option for 'heavy' flood with bloated packet sizes
             attrs.Continous = true;
+            
+            Ping p = new Ping(attrs, cancellationToken, ResultsUpdateCallback);
 
             // Disable output for faster speeds
             Display.ShowOutput = false;
 
-            // This callback will run after each ping iteration
-            void ResultsUpdateCallback(PingResults r)
-            {
-                // Make sure we're not updating the display too frequently
-                if (!displayUpdateLimiter.RequestRun()) {
-                    return;
-                }
-
-                // Calculate pings per second
-                double pingsPerSecond = 0;
-                if (displayUpdateLimiter.ElapsedSinceLastRun != TimeSpan.Zero) {
-                    pingsPerSecond = (r.Sent - previousPingsSent) / displayUpdateLimiter.ElapsedSinceLastRun.TotalSeconds;
-                }
-                previousPingsSent = r.Sent;
-
-                // Update results text
-                Display.FloodProgress(r.Sent, (ulong)Math.Round(pingsPerSecond), address);
-            }
-
             // Start flooding
-            PingResults results = p.Send(attrs, ResultsUpdateCallback);
+            PingResults results = p.Send();
 
             // Display results
             Display.ShowOutput = true;
             Display.PingResults(attrs, results);
         }
+
+        // This callback will run after each ping iteration
+        private static void ResultsUpdateCallback(PingResults r)
+        {
+            // Make sure we're not updating the display too frequently
+            if (!displayUpdateLimiter.RequestRun()) {
+                return;
+            }
+
+            // Calculate pings per second
+            double pingsPerSecond = 0;
+            if (displayUpdateLimiter.ElapsedSinceLastRun != TimeSpan.Zero) {
+                pingsPerSecond = (r.Sent - previousPingsSent) / displayUpdateLimiter.ElapsedSinceLastRun.TotalSeconds;
+            }
+            previousPingsSent = r.Sent;
+
+            // Update results text
+            Display.FloodProgress(r.Sent, (ulong)Math.Round(pingsPerSecond), m_Address);
+        }
+
     }
 }
