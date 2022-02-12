cf153e6df95b8bb71eff91722cec6f0287e56917
Matthew Carney <matthewcarney64@gmail.com>
2022-02-12 22:30:20 +0000

    Removed GetLinkerTime, functionality didn't work in dotnet 6

--- src/Utils/Helper.cs
+++ src/Utils/Helper.cs
@@ -153,38 +153,6 @@ namespace PowerPing
         }
 
         /// <summary>
-        /// Extension method for determining build time
-        /// </summary>
-        /// <param name="assembly"></param>
-        /// <param name="target"></param>
-        /// <source>http://stackoverflow.com/a/1600990</source>
-        /// <returns></returns>
-        public static DateTime GetLinkerTime(this Assembly assembly, TimeZoneInfo target = null)
-        {
-            //var filePath = assembly.Location;
-            //const int c_PeHeaderOffset = 60;
-            //const int c_LinkerTimestampOffset = 8;
-
-            //var buffer = new byte[2048];
-
-            //using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read)) {
-            //    stream.Read(buffer, 0, 2048);
-            //}
-
-            //var offset = BitConverter.ToInt32(buffer, c_PeHeaderOffset);
-            //var secondsSince1970 = BitConverter.ToInt32(buffer, offset + c_LinkerTimestampOffset);
-            //var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-            //var linkTimeUtc = epoch.AddSeconds(secondsSince1970);
-
-            //var tz = target ?? TimeZoneInfo.Local;
-            //var localTime = TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, tz);
-
-            //return localTime;
-            return new DateTime();
-        }
-
-        /// <summary>
         /// Checks if a string is a valid IPv4 address
         /// </summary>
         /// <param name="address"></param>
