f8b0d137a204a48fe1a6a2d1df7d90479e9ec0f6
Matthew Carney <matthewcarney64@gmail.com>
2025-12-28 12:26:35 +0000

    Pre-emptive bump of copyright year to 2026

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Matthew Carney")]
 [assembly: AssemblyProduct("PowerPing")]
-[assembly: AssemblyCopyright("Copyright © Matthew Carney 2024")]
+[assembly: AssemblyCopyright("Copyright © Matthew Carney 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
